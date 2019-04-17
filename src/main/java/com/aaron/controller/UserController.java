18bf49d66fceb2f68c486b2878f0776c1f4d5071
fishman45 <361647454@qq.com>
2019-04-17 17:54:15 +0800

    添加thymeleaf

--- src/main/java/com/aaron/controller/UserController.java
+++ src/main/java/com/aaron/controller/UserController.java
@@ -7,11 +7,13 @@ import com.aaron.services.UserService;
 import com.aaron.utils.LogUtil;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -22,21 +24,47 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // 测试mybatis后台读取数据
+    /**
+     * 测试mybatis后台读取数据
+     *
+     * @param id
+     * @return
+     */
     @ResponseBody
     @RequestMapping("getUser/{id}")
     public String getUser(@PathVariable int id) {
         return userService.getUser(id).toString();
     }
 
-    // 测试tkmybatis后台读取数据
+    /**
+     * 测试tkmybatis后台读取数据
+     *
+     * @return
+     */
     @ResponseBody
     @RequestMapping("getAll")
-    public List<User> getAll(@PathVariable int id) {
+    public List<User> getAll() {
         return userService.getAll();
     }
 
-    // 统一异常处理
+    /**
+     * thymeleaf获取数据
+     * @param model
+     * @return
+     */
+    @GetMapping
+    public ModelAndView getAll(Model model) {
+        model.addAttribute("userList", userService.getAll());
+        model.addAttribute("title","获取所有数据");
+        return new ModelAndView("list","userModel",model);
+    }
+
+    /**
+     * 统一异常处理
+     *
+     * @return
+     * @throws Exception
+     */
     @RequestMapping("/getError")
     public String GetError() throws Exception {
         Logger log1 = LogUtil.getBussinessLogger();
