6415256d6b484d7a444c9906def97642500feb42
361647454@qq.com <A.lflnl19901006>
2019-05-24 18:11:58 +0800

    springboot整合Swagger

---
+++ src/main/java/com/aaron/config/swagger/SwaggerConfig.java
@@ -0,0 +1,66 @@
+package com.aaron.config.swagger;
+
+import com.google.common.base.Predicate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.not;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * @author: lfl
+ * @description: 打开swagger2的url:http://localhost:8080/swagger-ui.html
+ * @date: Create in 2019/5/24 下午 02:40
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Value("${swagger.enable}")
+    private boolean swaggerEnable;
+
+    /***
+     * 配置swagger
+     * 开发和测试环境下可以开启swagger辅助进行调试,而生产环境下可以关闭或者进行相应的权限控制，防止接口信息泄露
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerEnable)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.aaron.controller"))
+                .paths(PathSelectors.any())
+                .paths(doFilteringRules())
+                .build();
+    }
+
+    /***
+     * 接口文档的描述信息
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("spring boot swagger2 用例")
+                .description("描述")
+                .licenseUrl("https://mit-license.org/")
+                .version("1.0")
+                .build();
+    }
+
+    /**
+     * 可以使用正则定义url过滤规则
+     */
+    private Predicate<String> doFilteringRules() {
+        return not(
+                regex("/ignore/*")
+        );
+    }
+}
--- src/main/java/com/aaron/controller/UserController.java
+++ src/main/java/com/aaron/controller/UserController.java
@@ -5,6 +5,8 @@ import com.aaron.entity.po.UserInfo;
 import com.aaron.enums.ResultEnum;
 import com.aaron.services.UserService;
 import com.aaron.utils.LogUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -22,6 +25,7 @@ import java.util.List;
  * @description: UserController层
  * @date: Create in 2019/4/21 上午 09:24
  */
+@Api(value = "用户接口", description = "用户信息接口")
 @Controller
 @RequestMapping("/testBoot")
 public class UserController {
@@ -34,6 +38,7 @@ public class UserController {
      * @return
      * @description: 测试mybatis后台读取数据
      */
+    @ApiOperation(notes = "查询某一用户", value = "用户接口")
     @ResponseBody
     @RequestMapping("/selectOne/{id}")
     @Cacheable(value = "selectOne")
@@ -46,6 +51,7 @@ public class UserController {
      * @return
      * @description: 测试tkmybatis后台读取数据
      */
+    @ApiOperation(notes = "查询所有用户", value = "用户接口")
     @ResponseBody
     @RequestMapping("/selectAll")
     @Cacheable(value = "selectAll")
@@ -58,6 +64,7 @@ public class UserController {
      * @return
      * @description: thymeleaf获取数据
      */
+    @ApiIgnore
     @RequestMapping("/list")
     public ModelAndView selectAll(Model model) {
         model.addAttribute("userList", userService.selectAll());
@@ -70,6 +77,7 @@ public class UserController {
      * @throws Exception
      * @description: 统一异常处理
      */
+    @ApiIgnore
     @RequestMapping("/getError")
     public String GetError() throws Exception {
         Logger businessLogger = LogUtil.getBusinessLogger();
