d4cd2692be3f842070c9c188cde3ae83052e115c
fishman45 <361647454@qq.com>
2019-05-27 12:40:15 +0800

    添加注解

--- src/main/java/com/aaron/entity/common/Druid.java
+++ src/main/java/com/aaron/entity/common/Druid.java
@@ -2,7 +2,6 @@ package com.aaron.entity.common;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author: lfl
@@ -13,19 +12,43 @@ import org.springframework.context.annotation.PropertySource;
 @ConfigurationProperties(prefix = "druid")
 public class Druid {
 
+    /**
+     * 数据库连接地址
+     */
     private String url;
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 
+    /**
+     * 驱动类
+     */
     private String driverClass;
 
+    /**
+     * 连接池的最大数据库连接数
+     */
     private int maxActive;
 
+    /**
+     * 最小空闲数，数据库连接的最小空闲时间
+     */
     private int minIdle;
 
+    /**
+     * 连接初始值,连接池启动时创建的连接数量的初始值
+     */
     private int initialSize;
 
+    /**
+     * 检测池里连接的可用性，生产环境一般是不开启的,主要是性能考虑
+     */
     private boolean testOnBorrow;
 }
